8a343beb6e7a764fb41797ffc8a345c41836aa17
cgeyer <clemens.geyer@gmail.com>
2016-05-20 15:05:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     username = request.args.get('user')
-	if (str(username) != '')
-	    return "Hello " + str(username)
-	else
+    if (str(username) != '')
+        return "Hello " + str(username)
+    else
         return "Hello everyone!"
-
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
